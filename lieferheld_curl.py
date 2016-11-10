1f1b3fd4f02808e461b8f48b81ded9af201ba9f1
ioliflou <nasia.ntalla@gmail.com>
2016-11-10 15:42:17 +0100

    Update lieferheld_curl.py

--- lieferheld_curl.py
+++ lieferheld_curl.py
@@ -10,7 +10,7 @@ reader = pd.read_csv(file, sep=';', encoding='utf-8-sig',dtype=str)
 zipcodes = reader["zipcode"].astype(str)
 base_url = "https://api.lieferheld.de/restaurants/?zipcode={zipcode}&fields=id&limit=4000"
 headers = {'Authentication': 'LH api-key=VXOEO5ezqBJIz0L6UAp46OjWSvGg7TA0DzdafBQ2Cf8g0Rbuu9FvOS9vE9BWmCMUzt63cPG4slTKR7apDF4WLnyy6xF1qmsxE6B80019ya0B292OgJmvK5rGhsxUVD96, token=2e113a99a9776fd78f6260d4e77f0555e1cd1985707c660762b45eb87ee9ff6e'}
-con = psycopg2.connect("dbname=marketing user=ba password=n8Ght_54aU34Dc_V19 host=bi-live-db1 port=5432")
+con = psycopg2.connect("dbname= user= password= host= port=")
 cur = con.cursor()
 cur.execute('CREATE TABLE delivery_zipcodes_api (zipcode text, restaurant_id text)')
 cur.execute('TRUNCATE TABLE delivery_zipcodes_api')
